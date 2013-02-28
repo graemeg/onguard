0fad38b2a6097a867dc787f3668121c1a2a5908b
Graeme Geldenhuys <graemeg@gmail.com>
2013-02-28 22:30:54 +0000

    Removed duplicate code that already exists in ogfile.pas

--- source/ogutil.pas
+++ source/ogutil.pas
@@ -250,19 +250,7 @@ function OgFormatDate(Value : TDateTime) : string;                     {!!.09}
 
 {file related routines}
 function GetFileSize(Handle : THandle) : Cardinal;
-{$IFNDEF Win32}
-function LockFile(hFile: THandle; dwFileOffsetLow, dwFileOffsetHigh: DWORD;
- nNumberOfBytesToLockLow, nNumberOfBytesToLockHigh: DWORD): Boolean;
-function UnlockFile(hFile: THandle; dwFileOffsetLow, dwFileOffsetHigh: DWORD;
- nNumberOfBytesToUnlockLow, nNumberOfBytesToUnlockHigh: DWORD): Boolean;
-{
-function LockFile(Handle : THandle; FileOffsetLow, FileOffsetHigh,
-                  LockCountLow, LockCountHigh : Word) : Boolean;
-function UnlockFile(Handle : THandle; FileOffsetLow, FileOffsetHigh,
-                    UnLockCountLow, UnLockCountHigh : Word) : Boolean;
-}
-function FlushFileBuffers(Handle : THandle) : Boolean;
-{$ENDIF}
+
 {$IFDEF LINUX}
 function GetDriveType(drive:Integer): Integer;
 function HiWord(I: DWORD):Word;
@@ -702,56 +690,7 @@ begin
   FileSeek(Handle, Save, 0);          {restore previous position}
 end;
 
-{$IFDEF LINUX}
-
-{
- these functions need to be fleshed out
- probably find similar functions in Libc
-}
-
-
-
-
-
-
-
-(*
-const
-  SEEK_SET        = 0;      { Seek from beginning of file.  }
-  SEEK_CUR        = 1;      { Seek from current position.  }
-  SEEK_END        = 2;      { Seek from end of file.  }
-
-{ Old BSD names for the same constants; just for compatibility.  }
-  L_SET           = SEEK_SET;
-  L_INCR          = SEEK_CUR;
-  L_XTND          = SEEK_END;
-*)
-
-
-{$ifdef FPC}
-const
-   F_RDLCK = 0;
-   F_WRLCK = 1;
-   F_UNLCK = 2;
-   F_EXLCK = 4;
-   F_SHLCK = 8;
-
-   LOCK_SH = 1;
-   LOCK_EX = 2;
-   LOCK_NB = 4;
-   LOCK_UN = 8;
-
-   LOCK_MAND = 32;
-   LOCK_READ = 64;
-   LOCK_WRITE = 128;
-   LOCK_RW = 192;
-
-   EACCES = ESysEACCES;
-   EAGAIN = ESysEAGAIN;
-{$endif}
-
-
-
+{$IFNDEF Win32}
 
 function GetLastError: Integer;
 begin
@@ -763,81 +702,7 @@ begin
   FpSetErrno(Value);
 end;
 
-function LockFile(hFile: THandle; dwFileOffsetLow, dwFileOffsetHigh: DWORD; nNumberOfBytesToLockLow, nNumberOfBytesToLockHigh: DWORD): Boolean;
-var
-  FLockInfo: {$ifdef FPC}BaseUnix.FLock{$else}TFLock{$endif};
-  FLastError: Cardinal;
-begin
-  Result := false;
-  FLockInfo.l_type := F_WRLCK;
-  FLockInfo.l_whence := SEEK_SET;
-  FLockInfo.l_start := dwFileOffsetLow;
-  FLockInfo.l_len := nNumberOfBytesToLockLow;
-  FLockInfo.l_pid := {$ifdef FPC}fpgetpid{$else}getpid{$endif}();
-  Result := {$ifdef FPC}fpfcntl{$else}fcntl{$endif}(hFile, F_SETLK, FLockInfo) <> -1;
-  if not Result then
-  begin
-    FLastError := GetLastError();
-    if (FLastError = EACCES) or (FLastError = EAGAIN) then
-      SetLastError(ESysEACCES)
-    else
-      Result := True; // If errno is ENOLCK or EINVAL
-  end;
-end;
-
-function UnlockFile(hFile: THandle; dwFileOffsetLow, dwFileOffsetHigh: DWORD; nNumberOfBytesToUnlockLow, nNumberOfBytesToUnlockHigh: DWORD): Boolean;
-var
-  FLockInfo: {$ifdef FPC}BaseUnix.FLock{$else}TFLock{$endif};
-begin
-  FLockInfo.l_type := F_UNLCK;
-  FLockInfo.l_whence := SEEK_SET;
-  FLockInfo.l_start := dwFileOffsetLow;
-  FLockInfo.l_len := nNumberOfBytesToUnLockLow;
-  FLockInfo.l_pid := {$ifdef FPC}fpgetpid{$else}getpid{$endif}();
-  Result := {$ifdef FPC}fpfcntl{$else}fcntl{$endif}(hFile, F_SETLK, FLockInfo) <> -1;
-end;
-
-
-
-{
-function LockFile(Handle : THandle;
-                  FileOffsetLow, FileOffsetHigh,
-                  LockCountLow, LockCountHigh : Word) : Boolean;
-var
-   LockVar : TFlock;
-begin
-  LockVar.l_whence := SEEK_SET;
-  LockVar.l_start := FileOffSetHigh;
-  LockVar.l_start := (LockVar.l_start shr 16) + FileOffSetLow;
-  LockVar.l_len := LockCountHigh;
-  LockVar.l_len := (LockVar.l_len shr 16) + LockCountLow;
-  LockVar.l_type := F_WRLCK;
-
-  if fcntl(Handle, F_SETLK, LockVar) = 0 then Result := True else Result := False;
-end;
-
-function UnlockFile(Handle : THandle;
-                    FileOffsetLow, FileOffsetHigh,
-                    UnLockCountLow, UnLockCountHigh : Word) : Boolean;
-var
-  LockVar : TFlock;
-begin
-  LockVar.l_whence := SEEK_SET;
-  LockVar.l_start := FileOffSetHigh;
-  LockVar.l_start := (LockVar.l_start shr 16) + FileOffSetLow;
-  LockVar.l_len := UnLockCountHigh;
-  LockVar.l_len := (LockVar.l_len shr 16) + UnLockCountLow;
-  LockVar.l_type := F_UNLCK;
-
-  if fcntl(Handle, F_SETLK, LockVar) = 0 then Result := True else Result := False;
-end;
-}
-
-function FlushFileBuffers(Handle : THandle) : Boolean;
-begin
-  Result := True;
-end;
-{$ENDIF}
+{$ENDIF not Win32}
 
 
 (*
